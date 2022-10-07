91e03fa6bad2b5e5a4d05cae5cc25c9ae897b4c8
Leopold Roos <leopold@leopold.se>
2022-10-07 20:43:02 +0200

    Extend NFT contract. Add an IpfsClient that proxies ipfs protocolls to https protocolls

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,5 +1,40 @@
 const { task } = require("hardhat/config");
 const { getContract } = require("./helpers");
+const fetch = require("node-fetch");
+
+const convertResult = async (result) => {
+  const text = await result.text();
+  try {
+    return JSON.parse(text);
+  } catch {
+    return text;
+  }
+};
+
+const handleResult = async (result) => {
+  try {
+    const data = await convertResult(result);
+    const reason = { external: true, error: data };
+    return result.ok ? Promise.resolve(data) : Promise.reject(reason);
+  } catch (error) {
+    const reason = { external: false, error };
+    return Promise.reject(reason);
+  }
+};
+
+const handleError = async ({ external, error }) => {
+  if (external) {
+    return Promise.reject({ location: "external (ipfsProxy site)", message: error });
+  }
+  const reason = "Oops! Something went wrong.";
+  return Promise.reject({ location: "internal (ipfsProxyClient related - Serverside or Client)", message: error || reason });
+};
+
+const ipfsProxyClient = async (ipfsUrl) => {
+  const url = `${ipfsUrl.replace("ipfs://", "https://w3s.link/ipfs/")}#x-ipfs-companion-no-redirect`;
+  return fetch(url).then(handleResult).catch(handleError);
+
+};
 
 task("mint", "Mints from the NFT contract")
   .addParam("address", "The address to receive a token")
@@ -9,4 +44,49 @@ task("mint", "Mints from the NFT contract")
       gasLimit: 500_000,
     });
     console.log(`Transaction Hash: ${transactionResponse.hash}`);
+  });
+
+task("set-base-token-uri", "Sets the base token URI for the deployed smart contract")
+  .addParam("baseUrl", "The base of the tokenURI endpoint to set")
+  .setAction(async function (taskArguments, hre) {
+    const contract = await getContract("NFT", hre);
+    const transactionResponse = await contract.setBaseTokenURI(taskArguments.baseUrl, {
+      gasLimit: 500_000,
+    });
+    console.log(`Transaction Hash: ${transactionResponse.hash}`);
+  });
+
+task("token-uri", "Fetches the token metadata for the given token ID")
+  .addParam("tokenId", "The tokenID to fetch metadata for")
+  .setAction(async function (taskArguments, hre) {
+    const contract = await getContract("NFT", hre);
+    // Note: this fails currently, due to not uploading sub-metadata-files as mentioned in https://docs.opensea.io/docs/part-3-adding-metadata-and-payments-to-your-contract
+    const response = await contract.tokenURI(taskArguments.tokenId, {
+      gasLimit: 500_000,
+    });
+    const metadata_url = response;
+    console.log(`Metadata URL: ${metadata_url}`);
+
+    let metadata;
+    if (metadata_url.indexOf("ipfs") === 0) {
+      metadata = await ipfsProxyClient(metadata_url);
+    } else {
+      metadata = await fetch(metadata_url).then(res => res.json());
+    }
+    console.log(`Metadata fetch response: ${JSON.stringify(metadata, null, 2)}`);
+  });
+
+task("base-token-uri", "Fetches the base token metadata")
+  .setAction(async function (taskArguments, hre) {
+    const contract = await getContract("NFT", hre);
+    const metadata_url = await contract.baseTokenURI();
+    console.log(`Metadata BASE_URL: ${metadata_url}`);
+
+    let metadata;
+    if (metadata_url.indexOf("ipfs") === 0) {
+      metadata = await ipfsProxyClient(metadata_url);
+    } else {
+      metadata = await fetch(metadata_url).then(res => res.json());
+    }
+    console.log(`Metadata fetch response: ${JSON.stringify(metadata, null, 2)}`);
   });
---
+++ services/ipfsProxyClient.js
@@ -0,0 +1,87 @@
+import { uuid } from "@/lib/common";
+
+const baseHeaders = { "Content-Type": "application/json" };
+const baseOptions = { headers: {}, body: {}, params: {}, stringify: true };
+
+const handleError = async ({ external, error }) => {
+  if (external) {
+    // console.error("Error in Slack: ", error);
+    return Promise.reject({ location: "external (ipfsProxy site)", message: error });
+  }
+
+  // Message for developer when Client crashes.
+  // console.error("Error in Client: ", error);
+
+  // Message for user when Client crashes
+  const reason = "Oops! Something went wrong.";
+  return Promise.reject({ location: "internal (ipfsProxyClient related - Serverside or Client)", message: error || reason });
+};
+
+const convertResult = async (result) => {
+  const text = await result.text();
+  try {
+    return JSON.parse(text);
+  } catch {
+    return text;
+  }
+};
+
+const handleResult = async (result) => {
+  try {
+    const data = await convertResult(result);
+    const reason = { external: true, error: data };
+    return result.ok ? Promise.resolve(data) : Promise.reject(reason);
+  } catch (error) {
+    const reason = { external: false, error };
+    return Promise.reject(reason);
+  }
+};
+
+const additionalHeaders = () => {
+  const xRequestId = uuid();
+
+  return {
+    "x-request-id": xRequestId,
+  };
+};
+
+const fetcher = async (
+  endpoint,
+  { method, headers = {}, body, params = {} } = baseOptions
+) => {
+  // Pass the ipfs address through a ipfs to https proxy:
+  // List of proxies can be found here: https://ipfs.github.io/public-gateway-checker/
+  const url = `${endpoint.replace("ipfs://", "https://w3s.link/ipfs/")}#x-ipfs-companion-no-redirect?${new URLSearchParams({
+    ...params,
+  })}`;
+  console.log("proxy url: ", url);
+  const options = {
+    method,
+    headers: { ...baseHeaders, ...additionalHeaders(), ...headers },
+    body: ["GET", "DELETE"].includes(method) ? null : JSON.stringify(body),
+  };
+  // console.log("NEWS:", options, url)
+  return fetch(url, options).then(handleResult).catch(handleError);
+};
+
+const post = (ipfsUrl, { headers, params, body } = baseOptions) =>
+  fetcher(ipfsUrl, {
+    method: "POST",
+    headers,
+    params,
+    body,
+  });
+
+const generic =
+  (method) =>
+    (ipfsUrl, { headers, params } = baseOptions) =>
+      fetcher(ipfsUrl, {
+        method: method,
+        headers,
+        params,
+      });
+
+export const ipfsProxyClient = {
+  get: generic("GET"),
+  post: post,
+};
