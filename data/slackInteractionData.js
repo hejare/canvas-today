32a4b4ec96572e2d94e40cad320556ef29c824b0
Leopold Roos <leopold@leopold.se>
2022-10-11 10:07:42 +0200

    Debug slack-interaction storaging

--- data/slackInteractionData.js
+++ data/slackInteractionData.js
@@ -1,7 +1,25 @@
 import { faunaDbClient, query } from "@/services/faunaDbClient";
+// import { downvoteHeadline, upvoteHeadline } from "data/headlineData";
 
-export const addInteraction = (data) => {
-  return faunaDbClient.query(
-    query.Create(query.Collection("slack-interaction"), { data })
+export const addInteraction = async (data) => {
+  // const id = ""; //data....
+  // const action = ""; //data....
+
+  // let result;
+  // switch (action) {
+  //   case "upvote":
+  //     result = await upvoteHeadline(id);
+  //     break;
+  //   case "downvote":
+  //     result = await downvoteHeadline(id);
+  //     break;
+  //   default:
+  //     throw new Error(`Unsupported action: ${action}`);
+  // }
+
+  const logResult = faunaDbClient.query(
+    query.Create(query.Collection("slack-interaction"), { data: JSON.stringify(data) })
   );
+  return logResult;
+  // return { result, logResult };
 };
