456176bc276eda5562e0428ba7b1e0f8202d5425
Leopold Roos <leopold@leopold.se>
2022-10-21 15:06:12 +0200

    Debug cors module not found issue at netlify

--- pages/api/headline/aggregate.js
+++ pages/api/headline/aggregate.js
@@ -1,16 +1,23 @@
 import { getToday } from "@/lib/common";
+import { cors, corsMiddleware } from "@/lib/corsMiddleware";
 import { fetchHeadline } from "@/lib/canvasToday";
 import { appendHeadlines } from "@/data/headlineData";
 import { STATUS_NOK_TEXT, STATUS_OK_TEXT } from "@/services/responseConstants";
 
 export default async function handler(req, res) {
+  await corsMiddleware(req, res, cors);
+
   const today = getToday();
   let meta = {
     timestamp: Date.now(),
     today,
   };
-
   try {
+    const { headline, original, filtered, headlineRegex } =
+      await fetchHeadline();
+    meta.headlines = { headline, original, filtered, headlineRegex };
+    meta.result = await appendHeadlines(filtered);
+
     res.status(200).json({
       status: STATUS_OK_TEXT,
       meta: meta,
