73e646116cc26f4c078a9b796c28b6bbaf9d005b
Leopold Roos <leopold@leopold.se>
2022-10-21 14:37:08 +0200

    Debug cors module not found issue at netlify

--- pages/api/health.js
+++ pages/api/health.js
@@ -2,7 +2,7 @@ import { cors, corsMiddleware } from "@/lib/corsMiddleware";
 
 export default async function handler(req, res) {
   await corsMiddleware(req, res, cors);
-  console.log("But wtf?");
+
   res.status(200).json({
     status: "ok",
     timestamp: Date.now(),
